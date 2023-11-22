d2ed160c659653d32042a127cb94bc2869534cc4
유수안 <awetumnn@gmail.com>
2023-11-22 17:54:42 +0900

    [ADD] 폰트파일 추가 및 폰트 전역으로 설정 - NanumSquareRound - establish Retrosans

--- src/main.js
+++ src/main.js
@@ -6,9 +6,14 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import InfiniteLoading from "v3-infinite-loading";
 import "v3-infinite-loading/lib/style.css";
 
+
 import App from "./App.vue";
 import router from "./router";
+import "@/assets/css/fonts.css"
+
 const app = createApp(App);
+
+
 
 app.use(createPinia());
 app.use(router);
