659b9ac633ee5e5c9dccab07207bccaa19beceb4
유수안 <awetumnn@gmail.com>
2023-11-23 14:50:39 +0900

    [FEAT] 전기차 충전소 Vue 구현 - localAxios() 수정

--- src/views/ElectricChargingStationView.vue
+++ src/views/ElectricChargingStationView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 import { listStations } from "@/api/estation";
-import { listSido, listGugun } from "@/api/map";
+import { listGugun } from "@/api/map";
 
 import VKakaoMap from "@/components/common/VKakaoMap.vue";
 import VSelect from "@/components/common/VSelect.vue";
@@ -9,7 +9,6 @@ import VSelect from "@/components/common/VSelect.vue";
 // const serviceKey = import.meta.env.VITE_OPEN_API_SERVICE_KEY;
 const { VITE_OPEN_API_SERVICE_KEY } = import.meta.env;
 
-const sidoList = ref([]);
 const gugunList = ref([{ text: "구군선택", value: "" }]);
 const chargingStations = ref([]);
 const selectStation = ref({});
@@ -23,28 +22,12 @@ const param = ref({
 
 onMounted(() => {
   // getChargingStations();
-  getSidoList();
+  getGugunList();
 });
 
-const getSidoList = () => {
-  listSido(
-    ({ data }) => {
-      let options = [];
-      options.push({ text: "시도선택", value: "" });
-      data.forEach((sido) => {
-        options.push({ text: sido.sidoName, value: sido.sidoCode });
-      });
-      sidoList.value = options;
-    },
-    (err) => {
-      console.log(err);
-    }
-  );
-};
 
-const onChangeSido = (val) => {
+const getGugunList = () => {
   listGugun(
-    { sido: val },
     ({ data }) => {
       let options = [];
       options.push({ text: "구군선택", value: "" });
@@ -60,7 +43,7 @@ const onChangeSido = (val) => {
 };
 
 const onChangeGugun = (val) => {
-  param.value.zscode = val;
+  param.value.zscode = val.substr(0,5);
   getChargingStations();
 };
 
@@ -83,45 +66,56 @@ const viewStation = (station) => {
 
 <template>
   <div class="container text-center mt-3">
-    <div class="alert alert-success" role="alert">전기차 충전소</div>
-    <div class="row mb-2">
-      <div class="col d-flex flex-row-reverse">
-        <VSelect :selectOption="sidoList" @onKeySelect="onChangeSido" />
-      </div>
-      <div class="col"><VSelect :selectOption="gugunList" @onKeySelect="onChangeGugun" /></div>
+    <h2 class="title-text">전기차 충전소 지도</h2>
+    <br>
+    <div class="mx-auto d-block" style="width: 360px;">
+      <div class="col d-flex justify-content-Center"><VSelect :selectOption="gugunList" @onKeySelect="onChangeGugun" /></div>
     </div>
+    <br>
     <VKakaoMap :stations="chargingStations" :selectStation="selectStation" />
-    <table class="table table-hover">
-      <thead>
-        <tr class="text-center">
-          <th scope="col">충전소명</th>
-          <th scope="col">충전소ID</th>
-          <th scope="col">충전기상태</th>
-          <th scope="col">위치</th>
-          <th scope="col">위도</th>
-          <th scope="col">경도</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          class="text-center"
-          v-for="station in chargingStations"
-          :key="station.statId + station.chgerId"
-          @click="viewStation(station)"
-        >
-          <th>{{ station.statNm }}</th>
-          <td>{{ station.statId }}</td>
-          <td>{{ station.stat }}</td>
-          <td>{{ station.addr }}</td>
-          <td>{{ station.lat }}</td>
-          <td>{{ station.lng }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <br>
+    <div class="card">
+      <table class="table table-hover">
+        <thead>
+          <tr class="text-center">
+            <th scope="col">충전소명</th>
+            <th scope="col">충전소ID</th>
+            <th scope="col">충전기상태</th>
+            <th scope="col">위치</th>
+            <th scope="col">위도</th>
+            <th scope="col">경도</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            class="text-center"
+            v-for="station in chargingStations"
+            :key="station.statId + station.chgerId"
+            @click="viewStation(station)"
+          >
+            <th>{{ station.statNm }}</th>
+            <td>{{ station.statId }}</td>
+            <td>{{ station.stat }}</td>
+            <td>{{ station.addr }}</td>
+            <td>{{ station.lat }}</td>
+            <td>{{ station.lng }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <style>
+.title-text{
+  font-family:'main';
+  color:#FFC436;
+}
+
+.card{
+  font-family: 'basic-L';
+}
+
 mark.purple {
   background: linear-gradient(to top, #c354ff 20%, transparent 30%);
 }
